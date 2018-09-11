d4a3c0024b279881f5a8103418ebaf3873593098
shilko2013 <shilko2013@gmail.com>
2018-09-11 09:23:01 +0300

    Version 1.1.1. init_dict_letters added.

--- dictionary.c
+++ dictionary.c
@@ -9,7 +9,8 @@
 #define TCHAR char
 #endif
 
-#define FILE_NAME_LENGHT 21
+#define FILE_NAME_LENGTH 21
+#define MAX_WORD_LENGTH 25
 
 /* Для быстрого поиска словарь разделен на 33 двумерных
  * массива(количество букв) - dictionary_by_letters.
@@ -18,7 +19,7 @@
  * Внутри dictionary_by_length слова отсортированы по алфавиту и
  * ищутся бинарным поиском. Имена файла пердставляются в виде
  * "dictionary_Lt_Le.txt", где Lt - номер буквы, начиная
- * с нуля, не считая буквы Ё, которая имеет номер 33, а Le - длина слова.
+ * с нуля [0;32], не считая буквы Ё, которая имеет номер 33, а Le - длина слова [2;25].
  * Каждое слово пишется с новой строки.
  */
 
@@ -29,12 +30,12 @@ typedef struct {
 } dictionary_by_length;
 
 typedef struct {
-    dictionary_by_length *dicts_length;
+    dictionary_by_length **dicts_length;
     int *size;
 } dictionary_by_letters;
 
 typedef struct {
-    dictionary_by_letters *dicts_letters;
+    dictionary_by_letters **dicts_letters;
     int *size;
 } dictionary;
 
@@ -42,26 +43,31 @@ typedef dictionary_by_length dict_length;
 
 typedef dictionary_by_letters dict_letters;
 
+int letter_to_int(const TCHAR *const letter, int rus) {
+    int letter_number = letter[0];
+    if (rus) { //манипуляции для русского языка
+        letter_number += 64; //so much magic constants :)
+        if (letter_number == -24) //буква Ё
+            letter_number = 33;
+    } else {
+        letter_number -= 65; // англ яз
+    }
+    return letter_number;
+}
+
 /*
  * @param letter - Прописная буква алфавита
  * @param length - Длина слов
  * @param rus - флаг русского языка
  */
-dict_length *init_dict_length(const TCHAR * const letter, const int length, const int rus) {
-    int letter_number = letter[0];
+dict_length *init_dict_length(const TCHAR *const letter, const int length, const int rus) {
+    int letter_number = letter_to_int(letter, rus);
     int language_length = length; //отвечает за unicode
-    if (rus) { //манипуляции для русского языка
-        letter_number += 64;
-        if (letter_number == -24) //буква Ё
-            letter_number = 33;
+    if (rus)                    //манипуляции для русского языка
         language_length *= 2; //16 бит на символ вместо 8
-    }
-    else {
-        letter_number -= 65; // англ яз
-    }
     dict_length *dict = (dict_length *) malloc(sizeof(dict_length));
-    TCHAR *path = (TCHAR *) malloc(sizeof(TCHAR) * FILE_NAME_LENGHT);
-    snprintf(path, sizeof(TCHAR) * FILE_NAME_LENGHT, "dictionary_%02d_%02d.txt", letter_number, length);
+    TCHAR *path = (TCHAR *) malloc(sizeof(TCHAR) * FILE_NAME_LENGTH);
+    snprintf(path, sizeof(TCHAR) * FILE_NAME_LENGTH, "dictionary_%02d_%02d.txt", letter_number, length);
     FILE *file = fopen(path, "r");
     if (file == NULL)
         return NULL;
@@ -90,11 +96,25 @@ dict_length *init_dict_length(const TCHAR * const letter, const int length, cons
     return dict;
 }
 
-dict_letters *init_dict_letter(const TCHAR letter) {
-    /*do {
+dict_letters *init_dict_letter(const TCHAR *letter, int rus) {
+    dict_letters *dict = (dict_letters *) malloc(sizeof(dict_letters));
+    dict->size = (int *) malloc(sizeof(int));
+    *dict->size = MAX_WORD_LENGTH;
+    dict->dicts_length = (dict_length **) malloc(sizeof(dict_length *) * MAX_WORD_LENGTH);
+    for (int i = 0; i < MAX_WORD_LENGTH; ++i) {
+        dict->dicts_length[i] = init_dict_length(letter, i, rus);
+    }
+    return dict;
+}
 
-    } while (init_dict_length(TCHAR letter, int length) != NULL);*/
-    return NULL;
+void print(dict_letters *d) {
+    for (int i = 0; i < MAX_WORD_LENGTH; ++i) {
+        if (d->dicts_length[i] != NULL)
+            for (int j = 0; j < *(d->dicts_length[i]->size); ++j) {
+                printf("%s\n", d->dicts_length[i]->words[j]);
+            }
+        printf("\n");
+    }
 }
 
 dictionary *init_dict() {
--- main.c
+++ main.c
@@ -2,9 +2,6 @@
 #include "field.c"
 
 int main() {
-    dict_length *dict = init_dict_length("Ó",5,1);
-    for (int i = 0; i < *dict->size; ++i) {
-        printf("%s\n",dict->words[i]);
-    }
+    print(init_dict_letter("Ó",1));
     return 0;
 }
